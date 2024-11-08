94fc764101241bf7c50a323dfad9802b41f55c33
aquila.ziedins <aquila.ziedins@student.kuleuven.be>
2024-11-08 21:55:22 +0100

    removed the dynamic publish topic in frontend

--- Nodejs/web-app/src/components/PublishMsg.tsx
+++ Nodejs/web-app/src/components/PublishMsg.tsx
@@ -4,16 +4,17 @@ import { useMqttClient } from "../mqtt/MqttContext";
 import { MqttActions } from "../mqtt/mqttProtocol";
 import { Components } from "../protobuf";
 
-const PublishMsg: React.FC<{cb: (topic: string) => void}> = ({cb}) => {
+const PUBLISH_TOPIC = 'event/test/frontend/Test';
+
+const PublishMsg = () => {
     const { publish } = useMqttClient();
 
-    const [publishTopic, setPublishTopic] = useState<string>('event/test/frontend/Test');
     const [message, setMessage] = useState<Test>({timestamp: new Date(), msg: 'Hello from the frontend!'});
 
     const onSubmit = () => {
         message.timestamp = new Date();
 
-        const topicSegments = publishTopic.split('/');
+        const topicSegments = PUBLISH_TOPIC.split('/');
 
         const action = topicSegments[0] as MqttActions;
         const target = topicSegments.slice(1, -2).join('/');
@@ -23,25 +24,8 @@ const PublishMsg: React.FC<{cb: (topic: string) => void}> = ({cb}) => {
         publish({topicPrefix: {action, target}, actorId, msgClass, payload: message});
     };
 
-    useEffect(() => {
-        cb(publishTopic);
-    }, [publishTopic, cb]);
-
     return (
         <div className="p-4 flex flex-col gap-3 bg-[#292b34]">
-            <div>
-                Publish topic:
-                <input 
-                    type="text" 
-                    className="bg-transparent border rounded-lg mx-2 px-1" 
-                    value={publishTopic} 
-                    onChange={(e) => setPublishTopic(e.target.value)}
-                    style={{
-                        width: `${Math.max(publishTopic.length + 1, 10)}ch`,
-                        minWidth: '100px'
-                    }}
-                />
-            </div>
             <div>
                 <h1 className="text-2xl mb-2 mt-4 font-bold">MqttMessage:</h1>
                 Message: 
--- Nodejs/web-app/src/components/Settings.tsx
+++ Nodejs/web-app/src/components/Settings.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import PublishMsg from "./PublishMsg";
 
 const Settings = () => {
-    const [publishTopic, setPublishTopic] = useState<string>('event/test/frontend/Test');
-    
     return (
         <div className="bg-[#272931] p-4">
             <div className='flex flex-col border rounded-lg text-xl text-left p-4 mb-10'>
@@ -23,13 +20,13 @@ const Settings = () => {
                 <div className='flex flex-col'>
                     <h3 className='mt-5 text-2xl'>NodeJs:</h3>
                     <ul>
-                        <li>- MsgClass: Test, SubscribeTopic: [event/testDotnet/#, event/testPython/#], PublishTopic: {publishTopic}</li>
+                        <li>- MsgClass: Test, SubscribeTopic: [event/testDotnet/#, event/testPython/#], PublishTopic: event/test/frontend/Test</li>
                     </ul>
                 </div>
             </div>
             <div className='flex flex-col text-xl text-left'>
 					<h2 className='text-4xl mb-8'>Publish message</h2>
-					<PublishMsg cb={(topic) => setPublishTopic(topic)}/>
+					<PublishMsg/>
             </div>
         </div>
     );
