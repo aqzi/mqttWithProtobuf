ef181170206e81ea55580074cc037a0666a93e24
aquila.ziedins <aquila.ziedins@student.kuleuven.be>
2024-11-08 22:54:07 +0100

    updated subscribeMsg to display user msg info

--- Nodejs/web-app/src/components/SubscribeMsg.tsx
+++ Nodejs/web-app/src/components/SubscribeMsg.tsx
@@ -1,10 +1,10 @@
-import { Test } from "../protobuf/Test";
+import { Test, User } from "../protobuf/Test";
 import useSubscription from "../mqtt/UseSubscription";
 import { useEffect, useRef, useState } from "react";
 import { MqttMessage } from "../mqtt/mqttProtocol";
 
 const SubscribeMsg = () => {
-    const [messages, setMessages] = useState<MqttMessage<Test>[]>([]);
+    const [messages, setMessages] = useState<MqttMessage<Test|User>[]>([]);
 
     const counter = useRef(0);
     
@@ -15,7 +15,14 @@ const SubscribeMsg = () => {
 
     useEffect(() => {
         if (message) {
-            const tmp = message as MqttMessage<Test>;
+            let tmp: MqttMessage<Test|User>;
+            
+            if (message.msgClass === 'Test') {
+                tmp = message as MqttMessage<Test>;
+            } else {
+                tmp = message as MqttMessage<User>;
+            }
+
             
             // Add the new message to the list of messages and make sure the list is not longer than 5 messages
             setMessages(prev => [tmp, ...prev].slice(0, 6));
@@ -23,6 +30,25 @@ const SubscribeMsg = () => {
             counter.current++;
         }
     }, [message]);
+
+    const PayloadInfo: React.FC<{msg: MqttMessage<Test|User>}> = ({msg}) => {
+        if (msg.msgClass === 'Test') {
+            const tmp = msg.payload as Test;
+
+            return (
+                <li className="text-blue-400">Message: {tmp.msg}</li>
+            );
+        } else {
+            const tmp = msg.payload as User;
+            
+            return (
+                <>
+                    <li className="text-blue-400">Name: {tmp.name}</li>
+                    <li className="text-blue-400">Age: {tmp.age}</li>
+                </>
+            );
+        }
+    }
     
     return (
         <div className="mt-20">
@@ -34,7 +60,8 @@ const SubscribeMsg = () => {
                             <li>Action: {msg.topicPrefix.action}</li>
                             <li>Actor: {msg.actorId}</li>
                             <li>Target: {msg.topicPrefix.target}</li>
-                            <li>Message: {msg.payload.msg}</li>
+                            <li>MsgClass: {msg.msgClass}</li>
+                            <PayloadInfo msg={msg}/>
                             <li>Timestamp: {msg.payload.timestamp?.toLocaleDateString()}, {msg.payload.timestamp?.toLocaleTimeString()}</li>
                             <li className="font-bold">Seq nr: {counter.current - idx} {idx === 0 ? ' --> [most recent]' : ''}</li>
                         </ul>
