27c8f961fbe6093e4cecbf2637f8496a422636f9
aquila.ziedins <aquila.ziedins@student.kuleuven.be>
2024-11-09 13:29:09 +0100

    updated readme

--- Python/src/main.py
+++ Python/src/main.py
@@ -8,67 +8,6 @@ import protobuf.Test_pb2 as test
 from datetime import datetime
 from dotenv import load_dotenv
 
-# # Callback on connection
-# def on_connect(client: mqtt.Client, userdata, flags, rc: int):
-#     print(f'Connected (Result: {rc})')
-
-#     # See: https://docs.solace.com/Open-APIs-Protocols/MQTT/MQTT-Topics.htm
-#     client.subscribe('foo')
-#     client.subscribe('hello/#')
-#     client.subscribe('languages/+')
-#     client.subscribe('game/+/move')
-
-#     test_msg = test.Test()
-#     test_msg.msg = 'Hello, World!'
-
-#     specific_datetime = datetime(2024, 11, 6, 12, 0, 0)
-#     test_msg.timestamp = specific_datetime
-
-#     msg = test_msg.SerializeToString()
-
-#     # Examples of publishing messages to different types of subscriptions
-#     client.publish('foo', payload=msg)
-#     client.publish('foo/foo', payload='will not be seen, no one is subscribed to this topic')
-
-#     client.publish('hello', payload='world')
-#     client.publish('hello/world/canada/ottawa', payload='# matches any level')
-
-#     client.publish('languages/python2', payload='eol')
-#     client.publish('languages/python3', payload='valid')
-#     client.publish('languages/other/level', payload='will not be seen, + only matches one level')
-
-#     client.publish('game/player1/move', payload='Moving to (1, 2)')
-#     client.publish('game/player2/move', payload='Moving to (3, 4)')
-
-
-# # Callback when message is received
-# def on_message(client: mqtt.Client, userdata, msg: mqtt.MQTTMessage):
-#     print(f'Message received on topic: {msg.topic}. Message: {msg.payload}')
-
-#     if msg.topic == 'foo':
-#         test_msg = test.Test()
-#         test_msg.ParseFromString(msg.payload)
-#         print(f'Parsed message: {test_msg.msg}, Timestamp: {test_msg.timestamp}')
-
-# def on_disconnect(client: mqtt.Client, userdata, rc: int):
-#     print(f'Disconnected (Result: {rc})')
-
-
-# # If using websockets (protocol is ws or wss), must set the transport for the client as below
-# client = mqtt.Client(transport='websockets')
-
-# client.on_connect = on_connect
-# client.on_message = on_message
-# client.on_disconnect = on_disconnect
-
-# client.username_pw_set('python', 'password')
-
-# # Certificate is required when using TLS endpoint (mqtts, wss, ssl)!
-# client.connect('localhost', port=8080, keepalive=5, bind_address='')
-
-# client.loop_forever()
-
-
 load_dotenv()
 
 def main():
--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # Mqtt with protobuf
 This project demonstrates how a dotnet, python and nodejs app can communicate with each other using protobuf messages over MQTT. For each of these 3 applications, I created a service that parses MQTT messages automaticly into the correct object to simplify further processing.
 
-## Quickstart
-1. Start mqtt broker using the docker compose
-2. Create a proto file in `devops/proto` and afterwards execute the sh file in `devops/scripts` to generate the protobuf files in each of the 3 applications.
+![Demo mqtt application](Nodejs/web-app//public//demoScreenshot.png)
+
+## 🔥 Quickstart
+1. Start mqtt broker using the docker compose.
+2. Execute the sh file in `devops/scripts` to generate the protobuf files in each of the 3 applications. Be aware that you also have to create an index.ts file inside `Nodejs/web-app/src/protobuf`! This can be done as follow:
+```typescript
+import { Test, User } from './Test'; //extend when adding more messages inside the Test.proto file
+//add additional imports when creating new proto files
+
+export const Components = {
+	Test,
+	User
+};
+```
+3. Go to Python project, create virtual env, install the packages with `pip install -r requirements` and run the code with `python Python/src/main.py`.
+4. Go to Dotnet project and run app with command `dotnet run`
+5. Go to Nodejs project, install packages with `npm install` and run the code with `npm start`.
+6. Go to `http://localhost:3000/` to view the demo mqtt application.
+
+## 🧑‍💻 Debugging
+You can debug each project. The settings are located in the .vscode directory. Make sure to adjust the 'python' property in case your virtual env is not located under the Python directory.
+
+## ✅ TODO
+- update the proto_generate.sh file such that it generates the index.ts file inside Nodejs/web-app/src/protobuf
+- make the mqtt service in all projects async
+- make a docker file for each project
+- create an sh file to start all projects at once
+- bug fix: nodejs app sometimes disconnects with the mqtt broker at start (temporary fix: refresh the page until it works)
